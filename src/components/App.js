7d363c132120a1fb0efd853ee410b3035eadd66e
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-07-30 18:03:18 +0100

    included css and imagecard components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import unsplash from "../api/unsplash.js";
 import SearchBar from "./SearchBar.js";
+import ImageList from "./ImageList.js";
 
 class App extends React.Component {
   state = { images: [] };
@@ -9,7 +10,7 @@ class App extends React.Component {
     const response = await unsplash.get("/search/photos", {
       params: { query: term },
     });
-
+    console.log(response.data.results);
     this.setState({ images: response.data.results });
   };
 
@@ -17,7 +18,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
