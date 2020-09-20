e7a28051fcc1d8c61dbcedf460ac28a7ef54d344
Peter Manchester <64981120+petermanchester1987@users.noreply.github.com>
2020-09-20 18:06:59 +0100

    Delete unsplash.js

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization: "Client-ID XYfuZhjFmp7vqNYJdSE6RxB6bxSg6dxZzrf59myw5RI",
-  },
-});
